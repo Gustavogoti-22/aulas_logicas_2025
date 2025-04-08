d3aa8c3f760430c277085423a78be4371011ba7d
Gustavo Flores <gustavo.f.lima10@aluno.senai.br>
2025-04-08 10:54:22 -0300

    Banch

--- git.js
+++ git.js
@@ -2,3 +2,6 @@ let comidas = ["lazanha", 'Pizza', 'Sushi', 'Hamburguer','Batata-Frita']
 console.log(`Todas as comidas`, comidas);
 comidas.sort()
 console.log(`comidas cordenadas: ${comidas}`);
+console.log(`comidas cordenadas: ${comidas}`);
+console.log(`comidas cordenadas: ${comidas}`);
+console.log(`teste`);
